be87d19dd64436145a3e69e82c630e5eb606a556
zajacignacy@gmail.com <zajacignacy@gmail.com>
2024-08-17 21:12:56 +0200

    Add unit test project

---
+++ Mafia/ViewModels/PlayerViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Mafia.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Mafia.ViewModels
+{
+    internal class PlayerViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public Player Player { get; set; }
+
+        public bool IsSelected
+        {
+            get
+            {
+                return isSelected;
+            }
+            set
+            {
+                isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private bool isSelected;
+
+        public PlayerViewModel(Player player)
+        {
+            this.Player = player;
+        }
+
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
